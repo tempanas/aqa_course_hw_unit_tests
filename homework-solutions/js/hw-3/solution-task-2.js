6ea478d0d433b3440b89303391c36f8020e0542a
tempanas <114049072+tempanas@users.noreply.github.com>
2025-09-02 01:05:46 +0300

    change Number to +

--- homework-solutions/js/hw-3/solution-task-2.js
+++ homework-solutions/js/hw-3/solution-task-2.js
@@ -2,8 +2,8 @@ const n = 5;
 const nStr = n.toString(); //преобразуем число в строку
 const nn = nStr + nStr;
 const nnn = nStr + nStr + nStr; //по задаче у нас n + nn + nnn, где n не перемножаются, а конкатенируются
-const nNum = Number(nStr);
-const nnNum = Number(nn);
-const nnnNum = Number(nnn);
+const nNum = +nStr;
+const nnNum = +nn;
+const nnnNum = +nnn;
 const sum = nNum + nnNum + nnnNum;
 console.log(sum);
