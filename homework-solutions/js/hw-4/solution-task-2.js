03ad1a249f4fb3f6b4b9ec5e6d38417a0cfb7603
tempanas <114049072+tempanas@users.noreply.github.com>
2025-09-02 01:25:58 +0300

    HW 4, solution for 2nd task

---
+++ homework-solutions/js/hw-4/solution-task-2.js
@@ -0,0 +1,116 @@
+const minAge = 18;
+const maxAge = 60;
+let age;
+
+// 1. Возраст 10
+age = 10;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+// Проверить задание со следующими значениями в переменной age: 17, 18, 19, 59, 60, 61
+console.log('---');
+// 2. Возраст 17
+age = 17;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+console.log('---');
+
+// 3. Возраст 18
+age = 18;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+console.log('---');
+
+// 4. Возраст 19
+age = 19;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+console.log('---');
+
+// 5. Возраст 59
+age = 59;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+console.log('---');
+
+// 6. Возраст 60
+age = 60;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+console.log('---');
+
+// 7. Возраст 61
+age = 61;
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else if (age < minAge) {
+  console.log("You don't have access cause your age is " + age + ". It's less than " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
+
+
+
+
